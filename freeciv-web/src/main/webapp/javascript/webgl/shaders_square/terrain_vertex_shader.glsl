e68f583bc6e9a78075a7213afa529396ab12fbc5
Andreas Røsdal <41650711+andreasrosdal@users.noreply.github.com>
2024-12-22 19:38:24 +0100

    Update terrain_vertex_shader.glsl to version 3

--- freeciv-web/src/main/webapp/javascript/webgl/shaders_square/terrain_vertex_shader.glsl
+++ freeciv-web/src/main/webapp/javascript/webgl/shaders_square/terrain_vertex_shader.glsl
@@ -1,39 +1,33 @@
-/**********************************************************************
-    Freeciv-web - the web version of Freeciv. http://www.FreecivX.net/
-    Copyright (C) 2009-2017  The Freeciv-web project
+#version 300 es
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+// Output to the fragment shader
+out vec2 vUv;
+out vec3 vNormal;
+out vec3 vPosition;
+out vec3 vPosition_camera;
+out vec3 vColor;
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+// Input from the vertex buffer and context
+in vec3 position;
+in vec3 normal;
+in vec2 uv;
+in vec3 vertColor;
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// Built-in uniforms for the transformation matrices
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
 
-***********************************************************************/
+void main() {
+    // Pass attributes to the fragment shader
+    vUv = uv;
+    vNormal = normal;
+    vPosition = position;
+    vColor = vertColor;
 
-varying vec2 vUv;
-varying vec3 vNormal;
-varying vec3 vPosition;
-varying vec3 vPosition_camera;
-varying vec3 vColor;
+    // Compute the model-view position
+    vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+    gl_Position = projectionMatrix * mvPosition;
 
-attribute vec3 vertColor;
-
-void main()
-{
-  vUv = uv;
-
-  vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
-  gl_Position = projectionMatrix * mvPosition;
-
-  vPosition_camera = gl_Position.xyz;
-  vNormal = normal;
-  vPosition = position;
-  vColor = vertColor;
+    // Pass the position in camera space
+    vPosition_camera = mvPosition.xyz;
 }
