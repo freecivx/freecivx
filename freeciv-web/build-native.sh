37b8f3b5f1815282a66bd8975e846e63496f1c78
Andreas Røsdal <41650711+andreasrosdal@users.noreply.github.com>
2025-02-03 21:10:29 +0100

    Update build-native.sh

--- freeciv-web/build-native.sh
+++ freeciv-web/build-native.sh
@@ -5,9 +5,14 @@ echo "Requires Python 3 and Pillow already installed."
 
 cp ../config/config.dist src/main/webapp/WEB-INF/config.properties
 mkdir -p src/derived/webapp
+
 cd ..
 cd scripts
+
 ./sync-js-hand.sh -f ../freeciv/freeciv -i /tmp/ -o ../freeciv-web/src/derived/webapp/ -d /tmp
+
+cd ..
+cd freeciv-web
 
 mvn clean package
 
