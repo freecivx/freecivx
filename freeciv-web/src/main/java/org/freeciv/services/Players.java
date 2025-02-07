a2f84c02e9247d51391709e6f136f86e6992af28
Andreas Røsdal <andreas.rosdal@gmail.com>
2025-02-07 21:53:01 +0100

    Update Players.java

--- freeciv-web/src/main/java/org/freeciv/services/Players.java
+++ freeciv-web/src/main/java/org/freeciv/services/Players.java
@@ -107,7 +107,7 @@ public class Players {
 
             query = "SELECT username, last_login, elo_rating from auth  "
                     + "WHERE verified = '1' and last_login > NOW() - INTERVAL 12 HOURS  "
-                    + "ORDER BY elo_rating DESC, last_login DESC";
+                    + "ORDER BY username DESC";
 
             statement = connection.prepareStatement(query);
             rs = statement.executeQuery();
